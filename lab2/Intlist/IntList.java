dd1948b8b754eab94efddc3a3f1cd7fe46ed7414
Muffasa <superwubbq@gmail.com>
2020-01-23 15:33:49 +0800

    lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,16 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(A==null && B==null) return null;
+        else if(A==null) return B;
+
+        IntList C = A;
+        while(C.rest!=null){
+            C = C.rest;
+        }
+        C.rest = B;
+        return A;
+
     }
 
     /**
@@ -91,7 +100,22 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        //return null;
+        if(A==null && B==null) return null;
+        else if(A==null) return B;
+
+        IntList newIntList = new IntList(A.first, null);
+        IntList ptr = newIntList;
+        A = A.rest;
+
+        while(A!=null){
+            ptr.rest = new IntList(A.first, null);
+            A = A.rest;
+            ptr = ptr.rest;
+        }
+        ptr.rest = B;
+
+        return newIntList;
     }
 
 
